60f82d52eb6541eab171d5bf69f1c5f662001c7f
acidbox <chloegamr2009@gmail.com>
2025-07-13 21:47:03 -0300

    a

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ initjson();
 
 const jsonfile = new Request("items.json");
 const listing = document.getElementById("listingg");
-const results = document.getElementById("contentlist");
+const results = document.getElementById("listingg");
 const u = new Audio('uuu.ogg');
+const lancer = new Audio('splat.ogg');
 var animate = true;
 var section = "greetmsg";
 
@@ -28,7 +29,10 @@ function femty() {
   div.appendChild(femty);
   console.log("CONGRATULATIONS!!!!!!!! yOU JST HIT THE 1 IN 12k CHANCE TO BE [[[femty]]] JMPSACERD!!!!!!!!!!! feel free to like. screen shot this. and umm just pin it on your wall or something... or you probably just ran the 'femty()' command now that i think about it... hm... this is awkward... if you DID do that do NOT pin this in your wall, you shameless CHEATER!!!!!!!!");
 }
+
 async function start() {
+  document.getElementById("listingg").innerHTML = "";
+  section = "greetmsg";
   for (const sel of data.quotes) {
     let rngsubtitle = pickRandomProperty(sel.nmpinla); // LET ME TELL YOU, I'M OUT HERE FROM A VERY FAR AWAY PLACE ALL FOR A CHANCE TO BE A STAR NOWHERE SEEMS TO BE TOO FAR 🗣️🗣️🗣️🔥🔥🔥🔥
 
@@ -47,14 +51,21 @@ async function start() {
     item.setAttribute("id", `${(sel.section)}`);
     listing.appendChild(item);
   }
+
   document.getElementById('greetmsg').setAttribute("class", "selecteditem");
   for (const sel of data[section]) { // go through every section namee
     const item = document.createElement("li");
     item.appendChild(document.createElement("span")).textContent = sel.name;
-    results.appendChild(item);
+
     if (typeof sel.link !== 'undefined') {
       item.setAttribute("onmousedown", `location.href=\'${(sel.link)}\';`);
     }
+
+    if (typeof sel.exec !== 'undefined') {
+      item.setAttribute("onmousedown", `${(sel.exec)}`);
+    }
+
+    results.appendChild(item);
   }
 }
 
@@ -63,41 +74,57 @@ async function btndisplay(choice) {
     document.getElementById(section).setAttribute("class", "none");
     section = choice;
     document.getElementById(choice).setAttribute("class", "selecteditem");
-    document.getElementById('contentlist').innerHTML = "";
+    document.getElementById("listingg").innerHTML = "";
+
+    // back button func
+    const item = document.createElement("li");
+    item.appendChild(document.createElement("span")).textContent = "Back";
+    item.setAttribute("onmousedown", `start()`);
+    results.appendChild(item);
+
     for (const sel of data[choice]) { // do the below code for EVERY single element in the array stuff
       const item = document.createElement("li"); //create the element for us to put stuff in
+
       if (sel == 'greetmsg') {
         console.log(sel);
         item.appendChild(document.createElement("span")).textContent = sel.name;
         results.appendChild(item);
       }
+
       if (typeof sel.name !== 'undefined') {
         item.appendChild(document.createElement("span")).textContent = ' ' + sel.name + ' ';
+
         if (typeof sel.kmoji !== 'undefined') {
           let rngkaomoji = pickRandomProperty(sel.kaomojis);
           item.appendChild(document.createElement("strong")).textContent = sel.kaomojis[rngkaomoji];
         }
+
         if (typeof sel.tag !== 'undefined') {
           item.appendChild(document.createElement("strong")).textContent = sel.tag;
         }
+
         if (typeof sel.xoxo !== 'undefined') {
           item.setAttribute("style", "text-decoration: line-through")
         }
+
         if (typeof sel.link !== 'undefined') {
-          item.setAttribute("onmousedown", `if (confirm('would you like to go to ${(sel.link)}?') == true) {
+          item.setAttribute("onclick", `if (confirm('would you like to go to ${(sel.link)}?') == true) {
                 location.href=\'${(sel.link)}\';
               }`
           );
         }
+
         if (typeof sel.dl !== 'undefined') {
-          item.setAttribute("onmousedown", `if (confirm('would you like to download a file from \\n${(sel.link)}\\n') == true) {
+          item.setAttribute("onclick", `if (confirm('would you like to download a file from \\n${(sel.link)}\\n') == true) {
                 downloadFile('${(sel.link)}', '${(sel.filename)}');
               }`
           );
         }
+
         if (typeof sel.exec !== 'undefined') {
-          item.setAttribute("onmousedown", `${(sel.exec)}`);
+          item.setAttribute("onclick", `${(sel.exec)}`);
         }
+
         results.appendChild(item);
       }
     }
@@ -111,6 +138,25 @@ function pickRandomProperty(obj) {
     if (Math.random() < 1 / ++count)
       result = prop;
   return result;
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' || 'Return' || 'Q') {
+    start()
+  }
+})
+
+document.addEventListener('contextmenu', function(ev) {
+    ev.preventDefault();
+    start()
+    return false;
+}, false);
+
+function mwrtng() {
+  u.play();
+  setTimeout(() => {
+    location.href = "mwrtng";
+  }, 1300);
 }
 
 async function downloadFile(url, filename) { // https://muhimasri.com/blogs/how-to-save-files-in-javascript/

c92409ce9f117643984f7a6ecebf899a1428db05
stuxvii <chloegamr2009@gmail.com>
2026-02-18 21:06:56 -0300

    add consumables sum ts palm

--- game/game.js
+++ game/game.js
@@ -8,9 +8,7 @@ class ZodiacSign {
     }
 }
 
-showFamilyTree.addEventListener("click", function (e) {
-    infoBox.style.display = "flex";
-});
+let canInteract = true;
 
 var my_rng = new Math.seedrandom();
 
@@ -64,9 +62,6 @@ class Relation {
     }
 }
 
-const div = document.createElement("div");
-navbar.append(div);
-
 class Relative {
     constructor(person, relation) {
         this.person = person;
@@ -112,7 +107,7 @@ class Person {
         intelligence = rand_int(100),
         looks = rand_int(100),
         health = 80 + rand_int(20), // cant be unfair with everything
-        respect = (happiness + looks)/2,
+        strength = 80,
     } = {}) {
         this.age = age;
         this.money = money;
@@ -125,7 +120,14 @@ class Person {
         this.intelligence = intelligence;
         this.looks = looks;
         this.health = health;
-        this.respect = respect;
+        this.respect = (this.happiness + this.looks)/2;
+        this.strength = strength;
+
+        this.sweetness = rand_int(100) / 100;
+        this.spicyness = rand_int(100) / 100;
+        this.bitterness = rand_int(100) / 100;
+        this.crunchyness = rand_int(100) / 100;
+        this.softness = rand_int(100) / 100;
 
         this.careerPotential = Math.max(
             (this.intelligence * 1.5) + (this.looks * 0.5),
@@ -142,12 +144,16 @@ let your = new Person();
 
 let meter_elements = {};
 const stats = {
+    strength: "💪",
     respect: "🤝",
     happiness: "☺️",
     intelligence: "💡",
     health: "💊",
     looks: "👁️"
 };
+
+const div = document.createElement("div");
+navbar.append(div);
 
 Object.entries(stats).forEach(([key, icon]) => {
     const meter = document.createElement("span");
@@ -166,6 +172,131 @@ Object.entries(stats).forEach(([key, icon]) => {
     meter_elements[key] = meter;
 });
 
+class Occupation {
+    constructor({
+        needed_intelligence = 0.0,
+        income = 0,
+        name = "",
+    } = {}) {
+        this.needed_intelligence = needed_intelligence;
+        this.income = income;
+        this.name = name;
+    }
+}
+
+class Consumable {
+    constructor({
+        // palate
+        sweetness = 0.0,
+        spicyness = 0.0,
+        bitterness = 0.0,
+        
+        // texture
+        crunchyness = 0.0,
+        softness = 0.0,
+
+        // addictions
+        widthdrawal = 0.0,
+
+        price = 0,
+        name = "",
+    } = {}) {
+        this.sweetness = sweetness;
+        this.spicyness = spicyness;
+        this.bitterness = bitterness;
+        this.crunchyness = crunchyness;
+        this.softness = softness;
+        this.widthdrawal = widthdrawal;
+        this.price = price;
+        this.name = name;
+    }
+}
+
+const consumables = {
+    alfajor: new Consumable({
+        name: "Alfajor",
+        softness: 0.8,
+        sweetness: 0.6,
+        spicyness: 0.0,
+        bitterness: 0.0,
+        crunchyness: 0.3,
+        widthdrawal: 0.01,
+        price: 2
+    }),
+    potato_chips: new Consumable({
+        name: "Potato Chips",
+        softness: 0.1,
+        sweetness: 0.0,
+        spicyness: 0.2,
+        bitterness: 0.0,
+        crunchyness: 0.6,
+        widthdrawal: 0.01,
+        price: 2
+    }),
+    cigarettes: new Consumable({
+        name: "Cigarettes",
+        softness: 0.8,
+        sweetness: 0.0,
+        spicyness: 0.0,
+        bitterness: 0.75,
+        crunchyness: 0.1,
+        widthdrawal: 1.0,
+        price: 5
+    }),
+    beer: new Consumable({
+        name: "Beer",
+        softness: 1.0,
+        sweetness: 0.0,
+        spicyness: 0.0,
+        bitterness: 0.8,
+        crunchyness: 0.1,
+        widthdrawal: 0.1,
+        price: 5
+    }),
+};
+
+const shopItems = [consumables["potato_chips"], consumables["alfajor"], consumables["cigarettes"], consumables["beer"]];
+
+const infoBoxPanels = {
+    family: () => {
+        for (let entry in your.family) {
+            let person = your.family[entry]["person"];
+            let relation = your.family[entry]["relation"];
+            let new_entry = document.createElement("span");
+            new_entry.textContent = `${person.gender} ${Relation.getString(relation).toLowerCase()} - ${person.name} ${person.surname} - ${person.age}`;
+            infoBox.append(new_entry);
+        }
+    },
+    shop: () => {
+        for (let item in shopItems) {
+            let name = shopItems[item]["name"];
+            let price = shopItems[item]["price"];
+            let new_entry = document.createElement("button");
+            new_entry.textContent = `${name} - $${price}`;
+            new_entry.onclick = () => {
+                if (your.money > price) {
+                    let softness_match = shopItems[item].softness * your.softness;
+                    let sweetness_match = shopItems[item].sweetness * your.sweetness;
+                    let spicyness_match = shopItems[item].spicyness * your.spicyness;
+                    let bitterness_match = shopItems[item].bitterness * your.bitterness;
+                    let crunchyness_match = shopItems[item].crunchyness * your.crunchyness;
+                    let total_enjoyment = softness_match + sweetness_match + spicyness_match + bitterness_match + crunchyness_match;
+                    total_enjoyment *= 20;
+                    your.happiness += total_enjoyment;
+                    print(`You bought and consumed ${name}, it made you ${total_enjoyment}% happier`)
+                    canInteract = true;
+                } else {
+                    print(`You don't have enough money for ${name}, you need $${price-your.money} more`);
+                }
+                update_meters();
+            };
+            infoBox.append(new_entry);
+        }
+    },
+}
+
+let currentIBPanel = infoBoxPanels.family;
+
 function update_meters() {
     for (let key in meter_elements) {
         meter_elements[key].style.setProperty("--p", your[key]);
@@ -179,14 +310,15 @@ function update_meters() {
         infoBox.style.display = "none";
     });
     infoBox.append(close_btn);
-    for (let entry in your.family) {
-        let person = your.family[entry]["person"];
-        let relation = your.family[entry]["relation"];
-        let new_entry = document.createElement("span");
-        new_entry.textContent = `${person.gender} ${Relation.getString(relation).toLowerCase()} - ${person.name} ${person.surname} - ${person.age}`;
-        infoBox.append(new_entry);
-    }
-    yourInfo.textContent = your.name + " " + your.surname + " - §" + your.money;
+
+    currentIBPanel.call();
+
+    your.happiness = clamp(your.happiness, 0, 100);
+    your.intelligence = clamp(your.intelligence, 0, 100);
+    your.looks = clamp(your.looks, 0, 100);
+    your.health = clamp(your.health, 0, 100);
+
+    yourInfo.textContent = `${your.name} ${your.surname} - $${your.money}`;
 
     textContainer.scrollTo({
         top: textContainer.scrollHeight,
@@ -212,6 +344,8 @@ function begin() {
 
     mother.money = mother.careerPotential * 1000;
     father.money = father.careerPotential * 1000;
+    mother.strength = mother.health/2 + rand_int(mother.health/2);
+    father.strength = father.health/2 + rand_int(father.health/2);
 
     your.gender = 0 == rand_int(2) ? "male" : "female";
     your.name = your.gender == "male" ? boy_names[rand_int(boy_names.length)] : girl_names[rand_int(girl_names.length)];
@@ -220,6 +354,7 @@ function begin() {
     your.health = (father.health + mother.health) / 2;
     your.intelligence = (father.intelligence + mother.intelligence) / 2;
     your.looks = (father.looks + mother.looks) / 2;
+    your.strength = your.health/2 + rand_int(your.health/2);
 
     your.family.push({ person: father, relation: Relation.Parent });
     your.family.push({ person: mother, relation: Relation.Parent });
@@ -283,10 +418,8 @@ function processYearlyEvents(person) {
     person.health = clamp(person.health, 0, 100);
 }
 
-let canAgeUp = true;
-
 ageUp.addEventListener("click", function () {
-    if (!canAgeUp) return;
+    if (!canInteract) return;
     years_passed++
     your.age++
     for (let entry in your.family) {
@@ -297,6 +430,20 @@ ageUp.addEventListener("click", function () {
     processYearlyEvents(your);
     update_meters();
     tickingSFX.play();
+});
+
+showShop.addEventListener("click", function (e) {
+    if (!canInteract) return;
+    currentIBPanel = infoBoxPanels.shop;
+    update_meters();
+    infoBox.style.display = "flex";
+});
+
+showFamilyTree.addEventListener("click", function (e) {
+    if (!canInteract) return;
+    currentIBPanel = infoBoxPanels.family;
+    update_meters();
+    infoBox.style.display = "flex";
 });
 
 function presentChoice(description, options) {
@@ -310,14 +457,14 @@ function presentChoice(description, options) {
         btn.onclick = () => {
             opt.callback();
             choiceDiv.remove();
-            canAgeUp = true;
+            canInteract = true;
             update_meters();
         };
         choiceDiv.appendChild(btn);
     });
 
     textContainer.appendChild(choiceDiv);
-    canAgeUp = false;
+    canInteract = false;
 }
 
 const eventPool = [
@@ -368,7 +515,7 @@ const eventPool = [
         id: "bully_encounter",
         title: "Bully",
         description: () => "Someone named " + boy_names[rand_int(boy_names.length)] + " is harassing you.",
-        chance: 1,
+        chance: 0.5,
         minAge: 6,
         maxAge: 14,
         criteria: (p) => p.looks < 40 || p.respect < 40,
@@ -446,6 +593,41 @@ const eventPool = [
                         p.intelligence += 2;
                         print("The owner was so grateful! You feel like a saint.");
                     }
+                }
+            ]);
+        }
+    }),
+    new LifeEvent({
+        id: "school_over",
+        title: "Good morning.",
+        description: "You're done with school.",
+        chance: 1.,
+        minAge: 18,
+        maxAge: 18,
+        effect: (p) => {
+            presentChoice("Will you pursue higher education?", [
+                {
+                    text: "Get into university",
+                    callback: () => {
+                        print("Here ideally you'd have sum options but yeah TODO")
+                    }
+                    /*callback: () => {
+                        presentChoice("Choose a major", [
+                            {
+                                text: "Get into university",
+                                callback: () => {
+                                }
+                            },
+                            {
+                                text: "Take a break",
+                                callback: () => {}
+                            }
+                        ]);
+                    }*/
+                },
+                {
+                    text: "Take a break",
+                    callback: () => {}
                 }
             ]);
         }
